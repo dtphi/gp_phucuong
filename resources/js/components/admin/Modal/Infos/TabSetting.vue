95081af1475024a5e2362dc1fde8e6fcd8c63297
phidangthanh <dtphi.khtn@gmail.com>
2021-02-21 23:21:20 +0700

    add editor media manage

--- resources/js/components/admin/Modal/Infos/TabSetting.vue
+++ resources/js/components/admin/Modal/Infos/TabSetting.vue
@@ -3,12 +3,15 @@
         <div class="card-body">
             <div class="form-group row">
                 <label for="news_setting" class="col-sm-2 col-form-label">.....</label>
+                <div id="media-manager"></div>
             </div>
         </div>
     </transition>
 </template>
 
 <script>
+    require ('../../libs/mm/style.css');
+    import { MM } from 'com@admin/libs/mm/mm.min';
 
     export default {
         name: 'TabGeneralForm',
@@ -17,6 +20,19 @@
             settingData: {
                 type: Object
             }
+        },
+
+        mounted () {
+            new MM({
+                el: '#media-manager',
+                api: {
+                    baseUrl: 'http://localhost:8000/api',
+                    listUrl: 'list',
+                    downloadUrl: 'download',  // optional
+                    uploadUrl: 'upload',      // optional
+                    deleteUrl: 'delete'       // optional
+                }
+            });
         },
 
         methods: {},
