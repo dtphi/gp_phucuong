98722dc90db76e7ac8945f5b63b958ae8e1c42fc
phidangthanh <dtphi.khtn@gmail.com>
2021-03-25 16:34:43 +0700

    test route

--- resources/js/routes/front.js
+++ resources/js/routes/front.js
@@ -1,20 +1,37 @@
 import MainLayout from 'v@front/layouts/main';
 import HomePage from 'v@front/page_homes';
 
+//test
+import Home from 'v@front/home';
+import HomeLayout from 'v@front/layouts';
+
 import { config } from '../common/config';
 
 export default [
     {
         path: '',
-        component: HomePage,
-        name: 'home-page',
+        component: Home,
+        name: 'home',
         meta: {
-            layout: MainLayout,
+            layout: HomeLayout,
             role: 'public',
             show: {
                 footer: true
             },
             title: 'ログイン | ' + config.site_name
-        }
+        },
+        children: [{
+            path: 'home-page',
+            component: HomePage,
+            name: 'home-page',
+            meta: {
+                layout: MainLayout,
+                role: 'public',
+                show: {
+                    footer: true
+                },
+                title: 'ログイン | ' + config.site_name
+            },
+        }]
     }
 ];
