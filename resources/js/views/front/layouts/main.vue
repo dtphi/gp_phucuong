2d41a521ad0a2e19507bf6405cac4ccb89a07e58
phidangthanh <dtphi.khtn@gmail.com>
2021-04-04 02:41:48 +0700

    responsive layout

--- resources/js/views/front/layouts/main.vue
+++ resources/js/views/front/layouts/main.vue
@@ -20,6 +20,39 @@
 	};
 </script>
 
+<!-- <style lang="scss">
+	.wrapper-container {
+		font-size: 14px;
+
+		// Text overflow ellipsis on two lines
+		.ellipsis-three-lines {
+			display: -webkit-box;
+			-webkit-line-clamp: 3;
+			-webkit-box-orient: vertical;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
+
+		.ellipsis-two-lines {
+			display: -webkit-box;
+			-webkit-line-clamp: 2;
+			-webkit-box-orient: vertical;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
+	}
+
+	.img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+
+	h4, p {
+		margin-bottom: 0;
+	}
+</style> -->
+
 <style lang="scss">
 	.wrapper-container {
 		font-size: 14px;
@@ -40,6 +73,9 @@
 			overflow: hidden;
 			text-overflow: ellipsis;
 		}
+
+		
+		word-break: break-word;
 	}
 
 	.img {
