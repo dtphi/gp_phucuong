a451dc874513efffd541eca9e26daeafe241d95b
Dang Thanh Phi <dangthanhphi@Dangs-MacBook-Pro.local>
2021-04-18 12:10:58 +0700

    home page

--- app/Http/Controllers/Api/Front/Services/HomeService.php
+++ app/Http/Controllers/Api/Front/Services/HomeService.php
@@ -32,11 +32,4 @@ final class HomeService implements HomeModel
 
         return $query->get();
     }
-
-    public function getCategories($parent_id = 0) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) " .  
-        " WHERE c.parent_id = '" . (int)$parent_id . "' AND c.status = '1' ORDER BY c.sort_order, LCASE(cd.name)");
-
-		return $query->rows;
-	}
 }
