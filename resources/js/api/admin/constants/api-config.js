d7e48220f4b19af3c78cc275ecac8291bc9c7ae5
Dang Thanh Phi <dangthanhphi@Dangs-MacBook-Pro.local>
2021-04-25 22:01:33 +0700

    change define func

--- resources/js/api/admin/constants/api-config.js
+++ resources/js/api/admin/constants/api-config.js
@@ -2,6 +2,9 @@ let baseUrl = window.origin;
 if (process.env.NODE_ENV == "server") {
     baseUrl = 'http://haydesachnoipodcast.com';
 }
+if (process.env.NODE_ENV == "development") {
+    baseUrl = 'http://localhost:8000';
+}
 
 const ApiConfig = {
     baseURL: baseUrl,
