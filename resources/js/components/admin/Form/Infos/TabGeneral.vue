2aea2eaa634ccc37693102201c89576e73d25939
Dang Thanh Phi <dangthanhphi@Dangs-MacBook-Pro.local>
2021-05-27 21:56:50 +0700

    add font size

--- resources/js/components/admin/Form/Infos/TabGeneral.vue
+++ resources/js/components/admin/Form/Infos/TabGeneral.vue
@@ -152,6 +152,12 @@
         ACTION_UPDATE_INFO
     } from 'store@admin/types/action-types';
     import tinymce from 'vue-tinymce-editor';
+    /*tinymce.init({
+        
+        font_formats:
+            "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Oswald=oswald; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats",
+    });*/
+    console.log('tinymce', tinymce)
     import {
         fn_get_tinymce_langs_url
     } from '@app/api/utils/fn-helper';
@@ -168,7 +174,10 @@
             return {
                 options: {
                     language_url: fn_get_tinymce_langs_url('vi_VN'),
-                    height: "500"
+                    height: "500",
+                    toolbar2: "undo redo | styleselect | fontsizeselect | fontselect",
+                    font_formats:
+    "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats",
                 }
             };
         },
