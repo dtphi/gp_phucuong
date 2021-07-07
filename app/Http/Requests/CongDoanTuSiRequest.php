caf56f687a51e64b1ff9d806e1c8b2856d283241
phidangthanh <dtphi.khtn@gmail.com>
2021-07-07 22:23:27 +0700

    add request

--- app/Http/Requests/CongDoanTuSiRequest.php
+++ app/Http/Requests/CongDoanTuSiRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Auth;
 
-class GiaoHatRequest extends FormRequest
+class CongDoanTuSiRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -28,9 +28,9 @@ class GiaoHatRequest extends FormRequest
         $formData = $this->all();
 
         $formData['name']             = isset($formData['name']) ? $formData['name'] : null;
-        $formData['khu_vuc']        = isset($formData['khu_vuc']) ? htmlentities($formData['khu_vuc']) : null;
-        $formData['nguoi_quan_hat']             = isset($formData['nguoi_quan_hat']) ? $formData['nguoi_quan_hat'] : null;
-        $formData['phan_loai']             = isset($formData['phan_loai']) ? $formData['phan_loai'] : null;
+        $formData['dia_chi']             = isset($formData['dia_chi']) ? $formData['dia_chi'] : null;
+        $formData['dien_thoai']             = isset($formData['dien_thoai']) ? $formData['dien_thoai'] : null;
+        $formData['ghi_chu']        = isset($formData['ghi_chu']) ? htmlentities($formData['ghi_chu']) : null;
         $formData['active']           = isset($formData['active']) ? $formData['active'] : null;
         $formData['sort_id']          = isset($formData['sort_id']) ? $formData['sort_id'] : null;
         $formData['tag']              = isset($formData['tag']) ? $formData['tag'] : null;
--- app/Http/Requests/GiaoDiemRequest.php
+++ app/Http/Requests/GiaoDiemRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Auth;
 
 class GiaoDiemRequest extends FormRequest
 {
@@ -14,6 +15,32 @@ class GiaoDiemRequest extends FormRequest
     public function authorize()
     {
         return false;
+    }
+
+    /**
+     * @author : dtphi .
+     * Get data to be validated from the request.
+     *
+     * @return array
+     */
+    public function validationData()
+    {
+        $formData = $this->all();
+
+        $formData['name']             = isset($formData['name']) ? $formData['name'] : null;
+        $formData['ghi_chu']        = isset($formData['ghi_chu']) ? htmlentities($formData['ghi_chu']) : null;
+        $formData['dia_chi']             = isset($formData['dia_chi']) ? $formData['dia_chi'] : null;
+        $formData['active']           = isset($formData['active']) ? $formData['active'] : null;
+        $formData['sort_id']          = isset($formData['sort_id']) ? $formData['sort_id'] : null;
+        $formData['tag']              = isset($formData['tag']) ? $formData['tag'] : null;
+        $formData['meta_title']       = isset($formData['meta_title']) ? $formData['meta_title'] : null;
+        $formData['meta_description'] = isset($formData['meta_description']) ? $formData['meta_description'] : null;
+        $formData['meta_keyword']     = isset($formData['meta_keyword']) ? $formData['meta_keyword'] : null;
+        $formData['update_user']      = isset(Auth::user()->id) ? Auth::user()->id : 0;
+
+        $this->merge($formData);
+
+        return $this->all();
     }
 
     /**
