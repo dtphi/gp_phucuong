bd876afea2d56754ea85061b63225c27d832830c
maskcodex@outlook.com.vn <maskcodex@outlook.com.vn>
2021-07-30 16:06:11 +0700

    page linh muc detail

--- resources/js/views/front/page_linh_muc_details/index.vue
+++ resources/js/views/front/page_linh_muc_details/index.vue
@@ -52,7 +52,74 @@
             <main-content v-if="_isContentMain">
                 <template v-slot:before>
                     <!-- Html linh mục detail -->
-                    Nội dung thông tin Linh Mục
+                    <div class="header-linh-muc w-100">
+                        <span class="d-block line-header"></span>
+                        <div class="box-header">
+                            <div class="row">
+                                <div class="col-mobile col-3">
+                                    <span class="d-block img-header">
+                                        <img class="img rounded-circle" src="http://haydesachnoipodcast.com/storage/.tmb/thumb_730x410/Image/NewPicture/Tin-Giao-Phan/Thu-Keu-Goi.jpg" alt="">
+                                    </span>
+                                </div>
+                                <div class="col-mobile col-9 text-center text-header">
+                                    <h2>Trang thông tin linh mục</h2>
+                                    <h5>Giáo phận phú cường</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="detail-linh-muc w-100 mt-3">
+                        <div class="row">
+                            <div class="col-mobile col-3">
+                                <div class="row">
+                                    <p class="col-4 text-uppercase">Linh mục</p>
+                                    <div class="col-8 level-text">
+                                        <p class="text-uppercase">Tên thánh</p>
+                                        <p class="text-uppercase">Họ và tên cha</p>
+                                        <p class="text-uppercase">Chức vụ hiện tại</p>
+                                    </div>
+                                </div>
+
+                                <div class="bi-tich p-4 mt-3">
+                                    <h4 class="text-uppercase text-white text-center mb-3">Bí tích</h4>
+                                    <a class="d-block text-white" href="#">Bí tích Rửa Tội</a>
+                                    <a class="d-block text-white" href="#">Bí tích Thêm sức</a>
+                                    <a class="d-block text-white" href="#">Bí tích Truyền Chức</a>
+                                    <a class="d-block text-white" href="#">1. Phó Tế</a>
+                                    <a class="d-block text-white" href="#">2. Linh Mục</a>
+                                    <a class="d-block text-white" href="#">3. Giám Mục</a>
+                                </div>
+                            </div>
+                            <div class="col-mobile col-9">
+                                <span class="d-block avatar-img mt-3">
+                                    <img class="img" src="https://giaophanphucuong.org/Image/Picture/Logo/logo%20for%20web.png" alt="">
+                                </span>
+                                <div class="info-personal mt-5">
+                                    <h4 class="text-uppercase">Thông tin cá nhân</h4>
+                                    <p class="row">
+                                        <span class="col-mobile col-4">Ngày sinh: </span>
+                                        <span class="col-mobile col-4">Tại: </span>
+                                    </p>
+                                    <p class="row">
+                                        <span class="col-mobile col-4">Giáo xứ: </span>
+                                        <span class="col-mobile col-4">Giáo phận: </span>
+                                    </p>
+                                    <p>Tên cha: </p>
+                                    <p>Tên mẹ: </p>
+                                    <p>CMND: </p>
+                                    <p class="row">
+                                        <span class="col-mobile col-4">Ngày cấp: </span>
+                                        <span class="col-mobile col-4">Nơi cấp: </span>
+                                    </p>
+                                </div>
+
+                                <div class="maxim text-center">
+                                    <h4 class="tit-maxim">Châm ngôn đời linh mục</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </template>
             </main-content>
             <content-bottom v-if="_isContentBottom">
