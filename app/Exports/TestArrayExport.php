db0d01bcd40abfb0ae37300c4174e6b6431c9fd9
phidangthanh <dtphi.khtn@gmail.com>
2021-12-14 22:18:07 +0700

    test

--- app/Exports/TestArrayExport.php
+++ app/Exports/TestArrayExport.php
@@ -11,9 +11,11 @@ use PhpOffice\PhpSpreadsheet\Style\Border;
 use PhpOffice\PhpSpreadsheet\Style\Color;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use PhpOffice\PhpSpreadsheet\Worksheet\PageSetup;
+use Log;
 
 class TestArrayExport implements FromArray, WithStyles, WithEvents
-{ 
+{
+    private $fileRead = 'logs/test.log';
     /**
      * Num increment.
      */
@@ -23,9 +25,15 @@ class TestArrayExport implements FromArray, WithStyles, WithEvents
         'C_#' => 1,
         'D_#' => 1
     ];
-   
-    public function array():array
+
+    private $mapColKeys = ['A_#' => '', 'B_#' => 1, 'C_#' => 2, 'D_#' => 3, 'E_#' => 4, 'U_#' => 20, 'V_#' => 21];
+
+    public function array(): array
     {
+        //$content = $this->__readLogFile();
+        //$this->getTableHtmlVs1($content);
+        //$this->getTableHtml();
+
         return $this->__readLogFile();
         /*$testCol = [];
         for ($i = 1; $i <= 16384; $i ++) {
@@ -34,21 +42,211 @@ class TestArrayExport implements FromArray, WithStyles, WithEvents
         return [
             [],
             //$testCol,
-            ['a','c'],
-            ['d','e'],
-            ['','','','','','', 'abc']
+            ['a', 'c'],
+            ['d', 'e'],
+            ['', '', '', '', '', '', 'abc']
         ];
     }
 
-    private function __readLogFile():array
+    public function getTableHtmlVs1(array $content = array()): string
     {
-        $pathFile = storage_path('logs/test.log');
-        
-        if (!is_file($pathFile)) {
-            \File::put(storage_path('logs/test.log'), '');
+        $arrTables = [];
+        $tdNum = 30;
+        $tdColOneNum = 20;
+
+        $items = [];
+        for ($i = 0; $i <= $tdNum; $i++) {
+            $items[] = '';
         }
 
-        $content = \File::get(storage_path('logs/test.log'));
+        if (!empty($content)) {
+            foreach ($content as $rows) {
+                $tmpItems = $items;
+                $tmpRows = $rows;
+                $tmpItems['config'] = $rows['__config__'];
+
+                foreach ($tmpRows as $key => $value) {
+                    if (str_starts_with($key, '__empty')) { 
+                        $index = (int) preg_replace('/\D/', '', $key);
+                        $tmpItems[$index ? $index : 0] = $value;
+                    }
+                    
+                    unset($tmpRows[$key]);
+                }
+
+                $arrTables[] = $tmpItems;
+            }
+        }
+
+        $html = '';
+        foreach ($arrTables as $key => $row) {
+            $html .= '<tr>';
+            $config = $row['config'];
+            unset($row['config']);
+            $tmpRow = $row;
+            $colSpan = '';
+
+            foreach ($tmpRow as $key => $value) {
+
+                $classTd = 'td_empty';
+                $lastContentIndex = $config['lastContentIndex'];
+                if ($key == $lastContentIndex) {
+                    $classTd = 'td_content';
+                    if ($lastContentIndex < $tdColOneNum) {
+                        $colSpan = ($tdColOneNum - (int)$config['lastContentIndex']);
+                    } else {
+                        $colSpan = 10;
+                    }
+                }
+                if ($config['isNo'] && $config['isNo'] == $key) {
+                    $classTd = 'td_is_no_' . $config['isNo'];
+                }
+
+                if ($key <= $lastContentIndex) {
+                    $txtColSpan = 'colspan="' . $colSpan . '"';
+                    $html .= '<td class="' . $classTd . '" ' . $txtColSpan . '>' . $value . '</td>';
+                } else {
+                    if (($config['colContent'] == 1) && ($key < ($tdNum - $colSpan))) {
+                        $html .= '<td class="td_empty"></td>';
+                    }
+                }
+            }
+            $html .= '</tr>';
+        }
+        Log::info($html);
+        return $html;
+    }
+
+    public function getTableHtml(): string
+    {
+        $arrTables = [];
+        $tdNum = 30;
+        $tdColOneNum = 20;
+
+        $items = [];
+        for ($i = 0; $i <= $tdNum; $i++) {
+            $items[] = '';
+        }
+
+        $content = $this->__readLogFile();
+        if (!empty($content)) {
+            foreach ($content as $rows) {
+                $tmpItems = $items;
+                $tmpRows = $rows;
+                $tmpItems['config'] = $rows['__config__'];
+
+                foreach ($tmpRows as $key => $value) {
+                    if (str_starts_with($key, '__empty')) { 
+                        $index = (int) preg_replace('/\D/', '', $key);
+                        $tmpItems[$index ? $index : 0] = $value;
+                    }
+                    if ($key == '__empty') {
+                        $tmpItems[0] = $value;
+                    }
+                    if ($key == '__empty_1') {
+                        $tmpItems[1] = $value;
+                    }
+                    if ($key == '__empty_2') {
+                        $tmpItems[2] = $value;
+                    }
+                    if ($key == '__empty_3') {
+                        $tmpItems[3] = $value;
+                    }
+                    if ($key == '__empty_4') {
+                        $tmpItems[4] = $value;
+                    }
+                    if ($key == '__empty_5') {
+                        $tmpItems[5] = $value;
+                    }
+                    if ($key == '__empty_6') {
+                        $tmpItems[6] = $value;
+                    }
+
+                    if ($key == '__empty_21') {
+                        $tmpItems[21] = $value;
+                    }
+                    if ($key == '__empty_22') {
+                        $tmpItems[22] = $value;
+                    }
+
+                    unset($tmpRows[$key]);
+                }
+
+                $arrTables[] = $tmpItems;
+            }
+        }
+
+        $html = '';
+        foreach ($arrTables as $key => $row) {
+            $html .= '<tbody><tr>';
+            $config = $row['config'];
+            unset($row['config']);
+            $tmpRow = $row;
+            $colSpan = '';
+
+            foreach ($tmpRow as $key => $value) {
+
+                $classTd = 'td_empty';
+                $lastContentIndex = $config['lastContentIndex'];
+                if ($key == $lastContentIndex) {
+                    $classTd = 'td_content';
+                    if ($lastContentIndex < $tdColOneNum) {
+                        $colSpan = ($tdColOneNum - (int)$config['lastContentIndex']);
+                    } else {
+                        $colSpan = 10;
+                    }
+                }
+                if ($config['isNo'] && $config['isNo'] == $key) {
+                    $classTd = 'td_is_no_' . $config['isNo'];
+                }
+
+                if ($key <= $lastContentIndex) {
+                    $txtColSpan = 'colspan="' . $colSpan . '"';
+                    $html .= '<td class="' . $classTd . '" ' . $txtColSpan . '>' . $value . '</td>';
+                } else {
+                    if (($config['colContent'] == 1) && ($key < ($tdNum - $colSpan))) {
+                        $html .= '<td class="td_empty"></td>';
+                    }
+                }
+            }
+            $html .= '</tr></tbody>';
+        }
+
+        return $html;
+
+        // Display on the template blade.
+        /*
+            <style>
+                td.td_empty { width: -1%;}
+                table, th, td {
+                    width: -1%;
+                    border: 1px solid black;
+                    border-collapse: collapse;
+                }
+            </style>
+            <table>
+                <thead>
+                    <tr>
+                        <th colspan="21" style="width: 60%;">ケース(手順・操作など）</th>        
+                        <th colspan="10" style="width: 40%;">確認内容</th>
+                    </tr>
+                </thead>
+                <!-- star -->
+                {!! nl2br($htmlTable) !!}
+                <!-- end -->
+            </table>
+        */
+    }
+
+    private function __readLogFile(): array
+    {
+        $pathFile = storage_path($this->fileRead);
+
+        if (!is_file($pathFile)) {
+            \File::put(storage_path($this->fileRead), '');
+        }
+
+        $content = \File::get(storage_path($this->fileRead));
 
         $fileObject = new \SplFileObject('php://memory', 'r+');
         $fileObject->fwrite($content);
@@ -58,14 +256,96 @@ class TestArrayExport implements FromArray, WithStyles, WithEvents
 
         while ($fileObject->valid()) {
             $line = $fileObject->current();
-            $this->__setExportStructure($arrExportContent, $line);
+            $this->__setExportStructureVs1($arrExportContent, $line);
             $fileObject->next();
         }
 
         return $arrExportContent;
     }
 
-    private function __setExportStructure(&$arrExportContent = array(), $row = ''):array
+    private function __setExportStructureVs1(array &$arrExportContent = array(), string $row = ''): array
+    {
+        $arrRow = explode(' ', $row);
+        $arrTmpRow = [];
+        $rowNum = count($arrExportContent) + 2;
+
+        if (!empty($arrRow)) {
+            if (isset($arrRow[0])) {
+                $numberAdd = $arrRow[0];
+                unset($arrRow[0]);
+                $contentAdd = $arrRow;
+                if ($numberAdd == '#') {
+                    $this->__setRowStructure($arrTmpRow, ['A_#',  $contentAdd, $rowNum]);
+                    $this->incrementNum['A_#'] += 1;
+                    $this->incrementNum['B_#'] = 1;
+                    $this->incrementNum['C_#'] = 1;
+                    $this->incrementNum['D_#'] = 1;
+
+                    return $arrExportContent[] = $arrTmpRow;
+                }
+                if ($numberAdd == '##') {
+                    $this->__setRowStructure($arrTmpRow, ['B_#',  $contentAdd, $rowNum]);
+                    $this->incrementNum['B_#'] += 1;
+                    $this->incrementNum['C_#'] = 1;
+                    $this->incrementNum['D_#'] = 1;
+
+                    return $arrExportContent[] = $arrTmpRow;
+                }
+                if ($numberAdd === '###') {
+                    $this->__setRowStructure($arrTmpRow, ['C_#',  $contentAdd, $rowNum]);
+                    $this->incrementNum['C_#'] += 1;
+                    $this->incrementNum['D_#'] = 1;
+
+                    return $arrExportContent[] = $arrTmpRow;
+                }
+                if ($numberAdd === '####') {
+                    $this->__setRowStructure($arrTmpRow, ['D_#',  $contentAdd, $rowNum]);
+                    $this->incrementNum['D_#'] += 1;
+
+                    return $arrExportContent[] = $arrTmpRow;
+                }
+                if (in_array($numberAdd, ['-', '='])) {
+                    $this->__setRowStructure($arrTmpRow, ['E_#',  $contentAdd, $rowNum]);
+
+                    return $arrExportContent[] = $arrTmpRow;
+                }
+                if (in_array($numberAdd, ['+'])) {
+                    $this->__setRowStructure($arrTmpRow, ['V_#',  $contentAdd, $rowNum]);
+
+                    return $arrExportContent[] = $arrTmpRow;
+                }
+            }
+        }
+
+        return [];
+    }
+
+    private function __setRowStructure(array &$row = array(), array $content = array()): void 
+    {
+        if (array_key_exists($content[0], $this->mapColKeys)) {
+            $isNo = -1;
+            $indexContent = $this->mapColKeys[$content[0]];
+            $lastContentIndex = (int)$indexContent;
+            if (isset($this->incrementNum[$content[0]])) {
+                $isNo = (int)$indexContent;
+                $lastContentIndex += 1;
+                
+                $row['__empty' . $indexContent] = $this->incrementNum[$content[0]];
+                $row['__empty_' . ((int)$indexContent + 1)] = implode(' ', $content[1]);
+            } else {
+                $row['__empty_' . (int)$indexContent] = implode(' ', $content[1]);
+            }
+            
+            $row['__rowNum__'] = $content[2];
+            $row['__config__'] = [
+                'isNo' => $isNo,
+                'colContent' => ($lastContentIndex < 21) ? 1 : 2,
+                'lastContentIndex' => $lastContentIndex
+            ];
+        }
+    }
+
+    private function __setExportStructure(array &$arrExportContent = array(), string $row = ''): array
     {
         $arrRow = explode(' ', $row);
         $arrTmpRow = [];
@@ -80,6 +360,11 @@ class TestArrayExport implements FromArray, WithStyles, WithEvents
                     $arrTmpRow['__empty'] = $this->incrementNum['A_#'];
                     $arrTmpRow['__empty_1'] = implode(' ', $contentAdd);
                     $arrTmpRow['__rowNum__'] = $rowNum;
+                    $arrTmpRow['__config__'] = [
+                        'isNo' => 0,
+                        'colContent' => 1,
+                        'lastContentIndex' => 1
+                    ];
                     $this->incrementNum['A_#'] += 1;
 
                     return $arrExportContent[] = $arrTmpRow;
@@ -88,6 +373,11 @@ class TestArrayExport implements FromArray, WithStyles, WithEvents
                     $arrTmpRow['__empty_1'] = $this->incrementNum['B_#'];
                     $arrTmpRow['__empty_2'] = implode(' ', $contentAdd);
                     $arrTmpRow['__rowNum__'] = $rowNum;
+                    $arrTmpRow['__config__'] = [
+                        'isNo' => 1,
+                        'colContent' => 1,
+                        'lastContentIndex' => 2
+                    ];
                     $this->incrementNum['B_#'] += 1;
 
                     return $arrExportContent[] = $arrTmpRow;
@@ -96,6 +386,11 @@ class TestArrayExport implements FromArray, WithStyles, WithEvents
                     $arrTmpRow['__empty_2'] = $this->incrementNum['C_#'];
                     $arrTmpRow['__empty_3'] = implode(' ', $contentAdd);
                     $arrTmpRow['__rowNum__'] = $rowNum;
+                    $arrTmpRow['__config__'] = [
+                        'isNo' => 2,
+                        'colContent' => 1,
+                        'lastContentIndex' => 3
+                    ];
                     $this->incrementNum['C_#'] += 1;
 
                     return $arrExportContent[] = $arrTmpRow;
@@ -104,19 +399,34 @@ class TestArrayExport implements FromArray, WithStyles, WithEvents
                     $arrTmpRow['__empty_3'] = $this->incrementNum['D_#'];
                     $arrTmpRow['__empty_4'] = implode(' ', $contentAdd);
                     $arrTmpRow['__rowNum__'] = $rowNum;
+                    $arrTmpRow['__config__'] = [
+                        'isNo' => 3,
+                        'colContent' => 1,
+                        'lastContentIndex' => 4
+                    ];
                     $this->incrementNum['D_#'] += 1;
 
                     return $arrExportContent[] = $arrTmpRow;
                 }
-                if (in_array($numberAdd,['-', '='])) {
+                if (in_array($numberAdd, ['-', '='])) {
                     $arrTmpRow['__empty_4'] = implode(' ', $contentAdd);
                     $arrTmpRow['__rowNum__'] = $rowNum;
+                    $arrTmpRow['__config__'] = [
+                        'isNo' => 0,
+                        'colContent' => 1,
+                        'lastContentIndex' => 4
+                    ];
 
                     return $arrExportContent[] = $arrTmpRow;
                 }
-                if (in_array($numberAdd,['+'])) {
+                if (in_array($numberAdd, ['+'])) {
                     $arrTmpRow['__empty_21'] = implode(' ', $contentAdd);
                     $arrTmpRow['__rowNum__'] = $rowNum;
+                    $arrTmpRow['__config__'] = [
+                        'isNo' => 0,
+                        'colContent' => 2,
+                        'lastContentIndex' => 21
+                    ];
 
                     return $arrExportContent[] = $arrTmpRow;
                 }
@@ -133,10 +443,10 @@ class TestArrayExport implements FromArray, WithStyles, WithEvents
     {
         //XFD
         return [
-            BeforeExport::class  => function(BeforeExport $event) {
+            BeforeExport::class  => function (BeforeExport $event) {
                 $event->writer->setCreator('DangThanhPhi');
             },
-            AfterSheet::class    => function(AfterSheet $event) {
+            AfterSheet::class    => function (AfterSheet $event) {
                 $event->sheet->setOrientation(PageSetup::ORIENTATION_LANDSCAPE);
 
                 /*$event->sheet->styleCells(
