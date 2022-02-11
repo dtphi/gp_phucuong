823a947e77e21f06dcbc7ec5942eda656e5832b8
vince <phamhoangphuc72@gmail.com>
2022-02-11 10:05:06 +0700

    fix route

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,7 @@ Route::namespace('App\Http\Controllers\Api\Front')
     Route::get('/app/info/get-special-information', 'NewsController@showSpecialModuleList');
     Route::get('/app/get-data-module', 'ModuleController@showDataList');
     Route::get('/app/get-data-giao-xu', 'Base\ApiController@getGiaoXuList');
-    Route::get('/app/get-data-hanh', 'Base\ApiController@getNgayLeList');
+    Route::get('/app/get-data-hanh', 'Base\ApiController@getNgayLeList');// change
     Route::get('/app/get-data-giao-phan', 'Base\ApiController@getGiaoPhanList');
     Route::post('/app/get-data-giao-hat', 'Base\ApiController@getGiaoHatList');
     Route::post('/app/get-data-giao-xu-by-id', 'Base\ApiController@getGiaoXuListById');
