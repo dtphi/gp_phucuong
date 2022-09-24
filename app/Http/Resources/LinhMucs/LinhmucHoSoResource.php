42882359c7d2d99d738a23a355b2e66e35061d9b
phidangthanh <dtphi.khtn@gmail.com>
2022-09-24 20:39:50 +0700

    ho so linh muc

--- app/Http/Resources/LinhMucs/LinhmucHoSoResource.php
+++ app/Http/Resources/LinhMucs/LinhmucHoSoResource.php
@@ -42,7 +42,7 @@ class LinhmucHoSoResource extends JsonResource
                 'name' => $name,
                 'type' => 'file',
                 'url' => Storage::url($hoSo),
-                '_dir' => url('admin/linh-mucs/edit/1?_dir=' . $_dir . '/' . $name)
+                '_dir' => url('admin/linh-mucs/edit/' . $res->id . '?_dir=' . $_dir . '/' . $name)
               ];
             }
 
@@ -52,7 +52,7 @@ class LinhmucHoSoResource extends JsonResource
                 'name' => $name,
                 'type' => 'dir',
                 'url' => Storage::url($hoSo),
-                '_dir' => url('admin/linh-mucs/edit/1?_dir=' . $_dir . '/' . $name)
+                '_dir' => url('admin/linh-mucs/edit/' . $res->id . '?_dir=' . $_dir . '/' . $name)
               ];
             }
 
